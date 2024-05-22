14b7e38d048691add082324ee66f071fc4330702
svscagn <95577971+saverioscagnoli@users.noreply.github.com>
2024-05-22 19:47:48 +0100

    refactor: use the html audio element instead of audiobuffersourcenode

--- src/index.tsx
+++ src/index.tsx
@@ -1,271 +1,239 @@
-import React, { useCallback, useEffect, useMemo, useRef, useState } from "react";
-import {
-  createAnimationHandlers,
-  loadAndEmitAudioSource,
-  startAudioSource
-} from "./scripts";
-import { AudioState, AudioVisualizerProps } from "./types";
+import React, { useEffect, useMemo, useRef, useState, type FC } from "react";
+import { createAnimationHandlers, load } from "./scripts";
+import type { AudioState, AudioVisualizerProps } from "./types";
 
-const AudioVisualizer: React.FC<AudioVisualizerProps> = ({
+const AudioVisualizer: FC<AudioVisualizerProps> = ({
   src,
-  autoStart = true,
   audioState: audioStateProp,
   onAudioStateChange: onAudioStateChangeProp,
+  timeFactor = 1000,
   onTimeChange,
+  autoStart = true,
+  volume = 1,
   stagger = 1,
+  fftSize = 256,
+  playbackRate = 1,
   barWidth = (w, l) => w / l,
-  barHeight = h => h,
-  barColor = "green",
+  barHeight = h => h * 0.5,
+  barColor = (h, l, i) => `hsl(${(360 / l) * i}, ${h}%, 50%)`,
   spaceBetweenBars = 1,
-  fftSize = 2048,
-  customDrawFunction = (ctx, { x, canvasHeight, barWidth, barHeight }) => {
+  customDrawFunction = (ctx, { canvasHeight, barWidth, barHeight, x }) => {
     ctx.fillRect(x, canvasHeight - barHeight, barWidth, barHeight);
   },
-  onSourceLoaded: onSourceLoadedProp,
-  onSourceStarted,
+
+  onSourceLoaded,
   onSourceEnded,
-  ...props
+  onSourcePaused,
+  onSourcePlaying
 }) => {
   /**
-   * Create an audio context and an analyser node
+   * Create the audio context and analyser node
    */
   const [audioContext, analyser] = useMemo(() => {
     const audioContext = new AudioContext();
     const analyser = audioContext.createAnalyser();
-    analyser.fftSize = fftSize;
+
     return [audioContext, analyser];
-  }, [fftSize]);
+  }, []);
 
   /**
-   * The states are necessary to the component to work properly,
-   * but they don't need to be controlled. So, we use an internal state if not provided
+   * Create an internal state for the audio source,
+   * used if the user does not provide their own state
    */
   const [internalState, setInternalState] = useState<AudioState>("unset");
+
   const audioState = audioStateProp ?? internalState;
   const onAudioStateChange = onAudioStateChangeProp ?? setInternalState;
 
-  /**
-   * Keep track of the previous state to handle the audio state changes
-   */
-  const prevState = useRef<AudioState>("unset");
-
-  /**
-   * Create a ref to the canvas element and the audio source node
-   */
   const canvasRef = useRef<HTMLCanvasElement>(null);
-  const sourceRef = useRef<AudioBufferSourceNode | null>(null);
+  const audioRef = useRef<HTMLAudioElement | null>(null);
 
   /**
-   * Create a ref to the start and stop functions of the animation
-   * To be used with the createAnimationHandlers function, which returns them
+   * Create refs for the start and stop animation functions
+   * @see createAnimationHandlers in `scripts.ts`
    */
-  const startRef = useRef<(() => void) | null>(null);
-  const stopRef = useRef<(() => void) | null>(null);
+  const startAnimationRef = useRef<(() => void) | null>(null);
+  const stopAnimationRef = useRef<(() => void) | null>(null);
 
   /**
-   * Create a nice handle so we don't need to call
-   * startRef.current?.() and stopRef.current?.() every time
+   * Create handlers so every time we don't have to call
+   * `startAnimationRef.current!` and `stopAnimationRef.current!`
    */
   const [startAnimation, stopAnimation] = useMemo(
-    () => [startRef.current!, stopRef.current!],
-    [startRef.current, stopRef.current]
+    () => [startAnimationRef.current!, stopAnimationRef.current!],
+    [startAnimationRef.current, stopAnimationRef.current]
   );
 
   /**
-   * Keep track of the start and pause time to handle the timing of the audio
-   */
-  const startTime = useRef<number>(0);
-  const pauseTime = useRef<number>(0);
-
-  /**
-   * This function is called when the audio source is loaded.
-   * Sets the state to pending, so the audio state management useEffect can handle it.
-   */
-  const onSourceLoaded = useCallback(() => {
-    onSourceLoadedProp?.(sourceRef.current!);
-    onAudioStateChange("pending");
-  }, [onSourceLoadedProp, onAudioStateChange]);
-
-  /**
-   * See when the analyser node is created, and set the start and stop functions
+   * This useEffect hook is responsible for creating the animation
+   * When any prop that affects the animation is changed, this hook
+   * will reset the animation
    */
   useEffect(() => {
-    if (!canvasRef.current) return;
+    if (!canvasRef.current || !analyser) return;
 
-    const [stop, start] = createAnimationHandlers(
+    let alreadyRunning = false;
+
+    /**
+     * If there was an animation running and the audio is playing
+     * we should stop the animation and start it again to make changes
+     */
+    if (
+      startAnimationRef.current &&
+      stopAnimationRef.current &&
+      audioState === "playing"
+    ) {
+      stopAnimation();
+      alreadyRunning = true;
+    }
+
+    analyser.fftSize = fftSize;
+
+    const [start, stop] = createAnimationHandlers(
       analyser,
-      canvasRef.current.getContext("2d")!,
+      canvasRef.current,
       stagger,
       barWidth,
       barHeight,
       barColor,
+      spaceBetweenBars,
       customDrawFunction
     );
 
-    startRef.current = start;
-    stopRef.current = stop;
-  }, [analyser, stagger]);
-
-  /**
-   * When the src string changes, if there was a previous source, stop it prematurely,
-   * without invoking the onSourceEnded callback.
-   *
-   * Load the new source and set the sourceRef.current to the new source.
-   */
-  useEffect(() => {
-    if (sourceRef.current && prevState.current !== "pending") {
-      sourceRef.current.onended = null;
-      sourceRef.current.stop();
-      stopAnimation();
+    /**
+     * If the audio is playing, we should start the animation again
+     */
+    if (alreadyRunning) {
+      start();
     }
 
-    /**
-     * Reset the source to avoid problems with the previous source
-     */
-    sourceRef.current = null;
-    startTime.current = 0;
-    pauseTime.current = 0;
-    onTimeChange?.(0);
+    startAnimationRef.current = start;
+    stopAnimationRef.current = stop;
+  }, [analyser, stagger, fftSize, spaceBetweenBars, playbackRate]);
 
-    loadAndEmitAudioSource(
+  /**
+   * This useEffect hook is responsible for loading the audio source
+   * When the `src` prop is changed, this hook will load the new audio source
+   * and starting playing it if `autoStart` is true
+   */
+  useEffect(() => {
+    if (!canvasRef.current || !src) return;
+
+    if (audioRef.current) {
+      audioRef.current.pause();
+      audioRef.current.onended = null;
+      audioRef.current = null;
+
+      if (!autoStart) {
+        stopAnimation();
+      }
+    }
+
+    const audio = load(
       src,
       audioContext,
       analyser,
-      sourceRef,
-      onSourceEnded,
       onAudioStateChange,
-      onSourceLoaded
+      () => onSourceLoaded?.(audio),
+      () => onAudioStateChange("ended")
     );
+
+    audio.playbackRate = playbackRate;
+
+    audioRef.current = audio;
+
+    if (autoStart) {
+      audio.play();
+      onAudioStateChange("playing");
+    }
   }, [src]);
 
   /**
-   * Manage timing updates based on the audio context's current time
+   * Make sure the speed is changed when the `playbackRate` prop is changed
    */
   useEffect(() => {
+    if (!audioRef.current) return;
+
+    audioRef.current.playbackRate = playbackRate;
+  }, [playbackRate]);
+
+  /**
+   * Make sure the volume is changed when the `volume` prop is changed
+   */
+  useEffect(() => {
+    if (!audioRef.current) return;
+
+    audioRef.current.volume = volume;
+  }, [volume]);
+
+  /**
+   * This useEffect hook is responsible for updating the time elapsed
+   * When the audio is playing, we should update the time elapsed every `timeFactor`
+   * if the audio is paused or ended, we should stop updating the time elapsed
+   */
+  useEffect(() => {
+    if (!audioRef.current) return;
+
     let interval: NodeJS.Timeout | null = null;
 
-    if (audioState === "playing") {
-      interval = setInterval(() => {
-        const currentTime = audioContext.currentTime - startTime.current;
-        onTimeChange?.(+currentTime.toFixed(1));
-      }, 100);
-    } else if (audioState === "paused" || audioState === "ended") {
-      if (interval !== null) {
-        clearInterval(interval);
-        interval = null;
+    switch (audioState) {
+      case "playing": {
+        interval = setInterval(() => {
+          onTimeChange?.(audioRef.current?.currentTime ?? 0);
+        }, timeFactor / playbackRate);
+
+        break;
+      }
+
+      case "paused":
+      case "ended": {
+        void (interval && clearInterval(interval));
+
+        break;
       }
     }
 
     return () => {
-      if (interval !== null) {
-        clearInterval(interval);
-      }
+      void (interval && clearInterval(interval));
     };
-  }, [audioState, audioContext, startTime, pauseTime]);
+  }, [audioState, timeFactor, playbackRate]);
 
   /**
-   * Audio state management
+   * This useEffect hook is responsible for playing, pausing and ending the audio
+   * When the `audioState` prop is changed, this hook will play, pause or end the audio
    */
   useEffect(() => {
+    if (!audioRef.current) return;
+
     switch (audioState) {
-      /**
-       * If the audio state is pending, and the autoStart is true, start the audio source
-       */
-      case "pending": {
-        if (autoStart) {
-          startAudioSource(
-            sourceRef.current!,
-            startAnimation,
-            audioContext.currentTime,
-            startTime,
-            pauseTime
-          );
-          onSourceStarted?.();
-          onAudioStateChange("playing");
-        }
-        break;
-      }
-
       case "playing": {
-        /**
-         * If the previous state was pending, and autoStart is false, means that
-         * the user changed the state manually, so we have to start the audio source
-         */
-        if (prevState.current === "pending" && !autoStart) {
-          startAudioSource(
-            sourceRef.current!,
-            startAnimation,
-            audioContext.currentTime,
-            startTime,
-            pauseTime
-          );
-          onSourceStarted?.();
-        } else if (prevState.current === "ended") {
-          loadAndEmitAudioSource(
-            src,
-            audioContext,
-            analyser,
-            sourceRef,
-            onSourceEnded,
-            onAudioStateChange,
-            onSourceLoaded
-          ).then(() => {
-            /**
-             * This may seem counterintuitive, but it's necessary to restart the audio when the user wants
-             * If the autostart is true, it handles that automatically in the pending state,
-             * If not, we have to start the audio manually
-             */
-            if (!autoStart) {
-              startAudioSource(
-                sourceRef.current!,
-                startAnimation,
-                audioContext.currentTime,
-                startTime,
-                pauseTime
-              );
-              onSourceStarted?.();
-              onAudioStateChange("playing");
-            }
-          });
-        } else if (prevState.current === "paused") {
-          startAnimation();
-          startTime.current = audioContext.currentTime - pauseTime.current;
-        }
+        audioRef.current.play();
 
-        audioContext.resume();
+        onSourcePlaying?.(audioRef.current);
+        startAnimation();
         break;
       }
 
-      /**
-       * If the audio state is paused, stop the animation and suspend the audio context
-       */
       case "paused": {
+        audioRef.current.pause();
+
+        onSourcePaused?.(audioRef.current);
         stopAnimation();
-        pauseTime.current = audioContext.currentTime - startTime.current;
-        audioContext.suspend();
         break;
       }
 
-      /**
-       * If the audio state is ended, stop the animation.
-       */
       case "ended": {
-        stopAnimation();
-        onSourceEnded?.();
-        startTime.current = 0;
-        pauseTime.current = 0;
+        audioRef.current.pause();
+        audioRef.current.currentTime = 0;
 
+        onSourceEnded?.(audioRef.current);
+        stopAnimation();
         break;
       }
     }
+  }, [audioState]);
 
-    /**
-     * Update the previous state
-     */
-    prevState.current = audioState;
-  }, [audioState, startTime, pauseTime]);
-
-  return <canvas {...props} ref={canvasRef} />;
+  return <canvas ref={canvasRef} />;
 };
 
 export { AudioVisualizer };
