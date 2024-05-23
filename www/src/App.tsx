4db4b6a1da57c92873a79f379c1b8a7b52a3eed5
saverioscagnoli <svscagn@gmail.com>
2024-05-23 11:16:42 +0200

    start website

---
+++ www/src/App.tsx
@@ -0,0 +1,10 @@
+import { useState } from "react";
+import { AudioVisualizer } from "react-visual-audio";
+
+function App() {
+  const [count, setCount] = useState(0);
+
+  return <AudioVisualizer src="" />;
+}
+
+export default App;
